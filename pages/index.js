578c0e12e135abd9fa14bc96ee30dea895674e0b
Austin Calvelage <austin.calvelage@icloud.com>
2022-08-06 18:02:47 -0400

    TailWind Config/Test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>A Fullstack Todo App</h1>
+      <h1 className='text-red-400'>A Fullstack Todo App</h1>
     </div>
   )
 }
