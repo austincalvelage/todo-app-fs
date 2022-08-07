7feee2cae417014f2d841b456491daf56ac8321b
Austin Calvelage <austin.calvelage@icloud.com>
2022-08-06 20:10:42 -0400

    Bug Fixes for merge issue

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import { Header } from '../components/Header'
 import { TodoInput } from '../components/TodoInput'
 
 export default function Home() {
   return (
-    <div className='font-josefin-sans h-screen bg-hero-light-mobile sm:bg-hero-light-desktop bg-no-repeat bg-contain'>
+    <div className='font-josefin-sans h-screen px-6 bg-hero-light-mobile sm:bg-hero-light-desktop bg-no-repeat bg-contain'>
+      <Header />
       <TodoInput />
     </div>
   )
