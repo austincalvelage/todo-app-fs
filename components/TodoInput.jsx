8e335475724c61a8ebdc918d45fce604b63caca0
Austin Calvelage <austin.calvelage@icloud.com>
2022-08-07 00:37:22 -0400

    Edited Styling to match design

--- components/TodoInput.jsx
+++ components/TodoInput.jsx
@@ -1,10 +1,9 @@
 import { IconCheck } from './IconCheck'
 export const TodoInput = () => {
   return (
-    <form className="bg-white flex items-center gap-3 w-full px-5 py-4 rounded-md">
-      <input type="checkbox" name="inputCheckbox" disabled/>
-      <input type="text" placeholder='Create a new todo...' name='todoInput' className='bg-transparent text-xs' />
-      <IconCheck className="p-10 bg-black"/>
+    <form className="bg-white flex items-center gap-3 w-full px-5 py-3.5 rounded-md">
+      <input type="checkbox" name="inputCheckbox" className='h-5 w-5 rounded-full border-gray-400' disabled/>
+      <input type="text" placeholder='Create a new todo...' name='todoInput' className='bg-transparent text-xs border-none w-full p-0' />
     </form>
   )
 }
