3f2739c89be0ff4990bcb8abe5bf8414e0165f8d
Austin Calvelage <austin.calvelage@icloud.com>
2022-08-06 20:02:52 -0400

    file changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
+import { Header } from '../components/Header'
 import { TodoInput } from '../components/TodoInput'
 
 export default function Home() {
   return (
-    <div className='font-josefin-sans h-screen bg-hero-light-mobile sm:bg-hero-light-desktop bg-no-repeat bg-contain'>
+    <>
+    <Head />
+    <div className='font-josefin-sans h-screen px-6 bg-hero-light-mobile sm:bg-hero-light-desktop bg-no-repeat bg-contain'>
+      <Header />
       <TodoInput />
     </div>
+    </>
   )
 }
