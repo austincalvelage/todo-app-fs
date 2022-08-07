6099299b4d0610506f492f32ef4b00883bc15ea6
Austin Calvelage <austin.calvelage@icloud.com>
2022-08-07 19:37:38 -0400

    Added FilterTodo component.. Still needs logic.

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -2,9 +2,9 @@ import { IconCross } from "./IconCross"
 
 export const TodoItem = () => {
   return (
-    <div className="flex items-center gap-3 w-full px-5 py-3.5 rounded-t-md border-b-2 border-gray-400">
-      <input type="checkbox" name="inputCheckbox" className='h-5 w-5 rounded-full border-gray-400' />
-      <span className="mr-auto">Complete online javascript course</span>
+    <div className="flex items-center gap-3 w-full px-5 py-3.5 rounded-t-md border-b-[1px] border-[#E3E4F1]">
+      <input type="checkbox" name="inputCheckbox" className='h-5 w-5 rounded-full border-[#E3E4F1] hover:cursor-pointer' />
+      <span className="mr-auto text-xs text-[#494C6B]">Complete online javascript course</span>
       <IconCross />
     </div>
   )
