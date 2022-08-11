d2fae1b515d6cc9b71292775a5ed9fb991c414ea
Austin Calvelage <austin.calvelage@icloud.com>
2022-08-10 22:40:34 -0400

    Made added props to IconCross, added Nav to Header

--- components/IconCross.js
+++ components/IconCross.js
@@ -1,8 +1,8 @@
 
-export const IconCross = () => {
+export const IconCross = ({fill = '#494C6B', tailwindStyles, width='12', height='12' }) => {
   return (
-  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="hover:cursor-pointer">
-    <path fillRule="evenodd" clipRule="evenodd" d="M11.7851 0.471404L11.3137 0L5.89256 5.42115L0.471404 0L0 0.471404L5.42115 5.89256L0 11.3137L0.471404 11.7851L5.89256 6.36396L11.3137 11.7851L11.7851 11.3137L6.36396 5.89256L11.7851 0.471404Z" fill="#494C6B"/>
+  <svg width={width} height={height} viewBox="0 0 12 12" fill='none' xmlns="http://www.w3.org/2000/svg" className={`hover:cursor-pointer ${tailwindStyles}`}>
+    <path fillRule="evenodd" clipRule="evenodd" d="M11.7851 0.471404L11.3137 0L5.89256 5.42115L0.471404 0L0 0.471404L5.42115 5.89256L0 11.3137L0.471404 11.7851L5.89256 6.36396L11.3137 11.7851L11.7851 11.3137L6.36396 5.89256L11.7851 0.471404Z" fill={fill}/>
   </svg>
   )
 }
